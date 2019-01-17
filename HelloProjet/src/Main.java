8b5ca32d0b499252707fa62e7ae1ddeedaac7eb0
lilong <lilong_03@163.com>
2019-01-17 21:34:40 +0800

    我创建了一套完整的git

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.println("HelloWorld!");
         System.out.println("Hello Test");
 		System.out.println("hello notepad++");
+        System.out.println("一套完整的git,我很开心");
     }
 }
